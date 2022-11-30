e71a91ae4ab58ae65d9f31d1928e981e3a88e4f1
julias42 <artyom-zhulikov@yandex.ru>
2022-11-30 13:00:07 +0700

    lesson 1.1

--- src/components/func/Message.jsx
+++ src/components/func/Message.jsx
@@ -3,7 +3,10 @@ import styles  from './Message.module.css'
 
 export function Message(props){
   return(
+    <>
     <h1 className={styles.title}>{props.title}</h1> 
-    
+    <input type="text"/>
+    <button className={styles.button}>Submit</button>
+    </>
   )
 }
